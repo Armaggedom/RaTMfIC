45a097e401b7999fdf7fdfcf69562bee2315001e
bombbom <69651890+Armaggedom@users.noreply.github.com>
2021-05-10 11:37:44 -0300

    Add files via upload

--- RaTMfIC.cpp
+++ RaTMfIC.cpp
@@ -3,7 +3,8 @@
 	Modulo de Pesquisa e Teste para Comunicacao interna (MdPTpCI)
 */
 //AnalogIN
-#define Test A0
+#define VECTORINVERSION A0
+#define ACCELEROMETER A1
 //right movement
 #define Rmov7 7	//R = right motor
 #define Lmov2 2 //L = left motor
@@ -15,7 +16,6 @@
 #define Rvel 5
 //global variables
 bool stopRequest=0;
-bool vetordirection=1;
 //global function
 void SafetyShutdownSystem();
 //---------------------------predefinition code-----------------------------//
@@ -37,50 +37,82 @@ void setup() {
 //---------------------------Loop code--------------------------------------//
 void loop() {
 	//local variable
+	int aceleration=analogRead(ACCELEROMETER);
 	//define function
-	void VectorRectilinearInversion(int val); //function to reverse the acceleration vector
+	void VectorRectilinearInversion(int VectorValue, int aceleration); //function to reverse the acceleration vector
+	int Acelerometer(int aceleration);
 //-------------------program-------------------
-	int val=analogRead(Test);
-	val = map(val, 0, 675, 0, 1);
+	int VectorValue=analogRead(VECTORINVERSION);
+	VectorValue=map(VectorValue, 0, 675, 0, 1);
 	//calls the function to reverse the acceleration vector
-	VectorRectilinearInversion(val);
+	VectorRectilinearInversion(VectorValue, aceleration);
 	//define ΔV
-	analogWrite(Rvel, 255);
-	analogWrite(Lvel, 255);
+	analogWrite(Rvel, Acelerometer(aceleration));
+	analogWrite(Lvel, Acelerometer(aceleration));
 }
 /*					    --->
 	Acceleration vector: a m/s²
-							--->
+//							--->
 -----------------------------V code-----------------------------------------//
 */
-void VectorRectilinearInversion(int val) {
+int Acelerometer(int aceleration) {
+	aceleration=map(aceleration, 0, 1023, 0, 180);
+//	Serial.println(aceleration);
+	return aceleration;
+}
+bool key=1;
+void VectorRectilinearInversion(int VectorValue, int aceleration) {
+	
+	/*							-->	
+	vectorValue is direction the V
+	key is a key to block motor
+	*/
 	void ProgressiveRectilinearAcceleration();
 	void RetrogradeRectilinearAcceleration();	
-	Serial.println(vetordirection);
+	;
+	
 	//two-phase system
-	if(val == 0 && vetordirection == 1) {
+	if(VectorValue==0 and key==1) {
 		RetrogradeRectilinearAcceleration();
 	}
-	else if(val == 1 && vetordirection == 0) {
+	else if(VectorValue==1 and key==0) {
 		ProgressiveRectilinearAcceleration();
 	}
 	//transition phase
-	else if(val == 0 && vetordirection == 0) {
-		stopRequest=1;
-		SafetyShutdownSystem();
-		vetordirection=1;
+	else if(VectorValue==0 and key==0 or VectorValue==1 and key==1) {
+		//aceleration not_eq 0
+		// Airlock system
+		if(VectorValue==0 and key==0) {
+			stopRequest=1;
+			key=1;
+			SafetyShutdownSystem();
+		}
+		else if(VectorValue==1 and key==1) {
+			stopRequest=1;
+			key=0;
+			SafetyShutdownSystem();
+		}
+		else {
+			Serial.print("[VV-2] VectorValue and vetordirection ERROR\nVector value = ");
+			Serial.println(VectorValue);
+			Serial.print(" Vector directiom = ");
+			Serial.println(key);
+		};
 	}
-	else if(val == 1 && vetordirection == 1) {
-		stopRequest=1;
-		vetordirection=0;
-		SafetyShutdownSystem();
+/*	else if(NULL) {
+		movment equal to 0; rotation
 	}
-	else {
+*/	else {
 		//error escape
+		Serial.print("[VV-1] VectorValue and vetordirection ERROR\nVector value = ");
+		Serial.println(VectorValue);
+		Serial.print(" Vector directiom = ");
+		Serial.println(key);
 		SafetyShutdownSystem();
 	};
+	Serial.println(key);
 }
-/*							--->
+/*    						--->
 -----------------------------V code-----------------------------------------//
 */
 void ProgressiveRectilinearAcceleration() {
@@ -94,6 +126,7 @@ void ProgressiveRectilinearAcceleration() {
 }
 /*						    --->
 -----------------------------V*-1 code--------------------------------------//
+VectorRectilinearInversion switch
 */
 void RetrogradeRectilinearAcceleration() {
 	Serial.print("RetrogradeRectilinearAcceleration mode activated\n");
@@ -108,7 +141,7 @@ void RetrogradeRectilinearAcceleration() {
 void SafetyShutdownSystem() {
 	Serial.print("SafetyShutdownSystem activated\n");
 	if(stopRequest=1) {
-		//stop engines
+		//shutdown only engines
 		digitalWrite(Rmov7, 0);
 		digitalWrite(Rmov8, 0);
 		digitalWrite(Lmov4, 0);
